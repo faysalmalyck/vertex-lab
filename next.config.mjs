4c01b89f43d96f3b70f9e481ec92df64799a0712
Faysal Malick <246467902+faysalmalyck@users.noreply.github.com>
2026-07-13 22:59:08 +0500

    Deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "export",
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
